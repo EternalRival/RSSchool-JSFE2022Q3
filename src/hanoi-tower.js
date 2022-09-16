8d2750d65959a872b6c225ec28539a22d30fabc6
EternalRival <eternalrival8@gmail.com>
2022-09-16 21:34:11 +0300

    feat: add hanoi-tower solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,11 +14,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disks, turnsSpeed/* disksNumber, turnsSpeed */) {
+  /* throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here */
+
+  const turns = 2**disks-1
+  const seconds = Math.floor(turns/(turnsSpeed/60/60))
+  
+  return {turns, seconds}
 }
 
 module.exports = {
   calculateHanoi
 };
+
+/*
+*Implement the function calculateHanoi that accepts diskNumber and turnsSpeed parameters. 
+*diskNumber is a number of disks
+*turnsSpeed is the speed of moving discs (in turns per hour). Both parameters are numbers.
+
+*calculateHanoi function returns an object with 2 properties:
+
+*    turns (minimum number of turns to solve the puzzle)
+*    seconds (minimum number of seconds to solve the puzzle at a given turnsSpeed, seconds must be an integer, obtained from rounded down (floor) calculation result)
+
+ */
