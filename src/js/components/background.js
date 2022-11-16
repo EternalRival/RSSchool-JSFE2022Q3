3968e7bdb37454e699c83322913f55405345526f
EternalRival <eternalrival8@gmail.com>
2022-11-16 22:34:20 +0300

    feat: add poster image to video

--- src/js/components/background.js
+++ src/js/components/background.js
@@ -1,12 +1,18 @@
 import Element from '../class/Element';
-import video from '../../assets/matrix.mp4';
+import matrixImg from '../../assets/matrix.jpg';
+import matrixVideo from '../../assets/matrix.mp4';
 
 export default class BackgroundVideo extends Element {
   constructor() {
     super(document.body, 'video', 'bg-video');
-    Object.assign(this.el, { autoplay: true, loop: true, muted: true });
+    Object.assign(this.el, {
+      autoplay: true,
+      loop: true,
+      muted: true,
+      poster: matrixImg,
+    });
 
     const src = new Element(this.el, 'source');
-    Object.assign(src.el, { src: video, type: 'video/mp4' });
+    Object.assign(src.el, { src: matrixVideo, type: 'video/mp4' });
   }
 }
