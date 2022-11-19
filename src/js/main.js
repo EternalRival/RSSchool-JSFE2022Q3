ab4a0ac44fb7e7f032ab9a868cc25ae9b2d334f3
EternalRival <eternalrival8@gmail.com>
2022-11-19 07:38:14 +0300

    feat: add selfcheck console.log to main page

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,5 @@ const footer = new Footer();
 const startGame = new Anchor(main.el, './quiz', 'start-game main-menu interactive', 'Start Game');
 const gallery = new Anchor(main.el, './gallery', 'gallery main-menu interactive', 'Gallery');
 const result = new Anchor(main.el, './result', 'result main-menu interactive', 'Last Result');
+
+import('./checklist');
