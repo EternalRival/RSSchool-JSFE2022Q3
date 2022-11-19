3fc9af07ca65b2769acba59e0e49e565571223d8
EternalRival <eternalrival8@gmail.com>
2022-11-19 07:40:23 +0300

    feat: add _blank target to footer links

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -4,8 +4,20 @@ import Anchor from '../class/Anchor';
 export default class Footer extends Element {
   constructor() {
     super(document.body, 'footer', 'footer');
-    this.github = new Anchor(this.el, 'https://github.com/EternalRival', 'github interactive');
+    this.github = new Anchor(
+      this.el,
+      'https://github.com/EternalRival',
+      'github interactive',
+      '',
+      '_blank',
+    );
     this.year = new Element(this.el, 'div', 'year', '2022');
-    this.rssLogo = new Anchor(this.el, 'https://rs.school/js/', 'rss-logo interactive');
+    this.rssLogo = new Anchor(
+      this.el,
+      'https://rs.school/js/',
+      'rss-logo interactive',
+      '',
+      '_blank',
+    );
   }
 }
