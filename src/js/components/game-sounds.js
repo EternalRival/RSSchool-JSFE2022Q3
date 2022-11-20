42cebb97bb1d84f5793842f2b1c9de79a25fe630
EternalRival <eternalrival8@gmail.com>
2022-11-20 17:03:34 +0300

    feat: add independence of answer sounds

--- src/js/components/game-sounds.js
+++ src/js/components/game-sounds.js
@@ -1,7 +1,19 @@
 import correct from '../../assets/sounds/correct.mp3';
 import wrong from '../../assets/sounds/wrong.mp3';
 
+class Sound {
+  constructor(URI, options) {
+    Object.assign(this, { URI, options });
+  }
+
+  play() {
+    const sound = new Audio(this.URI);
+    Object.assign(sound, { ...this.options });
+    sound.play();
+  }
+}
+
 export default {
-  correct: new Audio(correct),
-  wrong: new Audio(wrong),
+  correct: new Sound(correct),
+  wrong: new Sound(wrong),
 };
