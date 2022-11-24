12d1a2aeaf788737b5a8734d52f0bee955d1438b
EternalRival <eternalrival8@gmail.com>
2022-11-24 03:59:56 +0300

    fix: linter error

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -73,7 +73,7 @@ function getAverage(value1, value2/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(x1, y1, x2, y2/* x1, y1, x2, y2 */) {
+function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
   throw new Error('Not implemented');
 }
 
@@ -204,7 +204,7 @@ function roundToPowerOfTen(num, pow/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n/* n */) {
+function isPrime(/* n */) {
   throw new Error('Not implemented');
 }
 
