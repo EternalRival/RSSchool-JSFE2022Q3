682aeeaf789c4e90def16864d6f0fee88ff3ded3
EternalRival <eternalrival8@gmail.com>
2022-12-07 03:18:36 +0300

    ci: add typescript to configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.ts'],
     },
     output: {
         filename: 'index.js',
