1b42834ab5725514b085cbe60a1c693eafc8e87e
EternalRival <eternalrival8@gmail.com>
2022-12-10 16:56:45 +0300

    feat: typify controller/appLoader

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,9 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '63383ad8c3164b8abdedaeb401f3bff8',
-        });
+        super('https://newsapi.org/v2/', { apiKey: '63383ad8c3164b8abdedaeb401f3bff8' });
     }
 }
 
