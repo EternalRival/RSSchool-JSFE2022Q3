c1b1702f65b871172f6224107df2deb6a80af3cb
EternalRival <eternalrival8@gmail.com>
2022-12-11 08:20:05 +0300

    feat: typify news.ts

--- src/components/view/news/news.ts
+++ src/components/view/news/news.ts
@@ -1,37 +1,58 @@
+import { IKek as IDrawSource, NewsItem, Source } from '../../interfaces/interfaces';
 import './news.css';
 
-class News {
-    draw(data) {
-        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+class News implements IDrawSource {
+    draw(data: NewsItem[]) {
+        /*
+        data: {
+            "source": {
+                         "id": "abc-news",
+                         "name": "ABC News"
+                      },
+                      "author": "The Associated Press",
+                      "title": "Scientists lower alert for Mauna Loa, say eruption could end",
+                      "description": "Scientists have lowered the alert level for the Mauna Loa volcano on Hawaii’s Big Island and say its first eruption in nearly 40 years may soon end",
+                      "url": "https://abcnews.go.com/US/wireStory/scientists-lower-alert-mauna-loa-eruption-end-94965247",
+                      "urlToImage": "https://s.abcnews.com/images/US/WireAP_97774604598f4e24a3e644191ea07f90_16x9_992.jpg",
+                      "publishedAt": "2022-12-11T03:03:59Z",
+                      "content": "HONOLULU -- Scientists lowered the alert level for the Mauna Loa volcano on Hawaii's Big Island from a warning to a watch on Saturday and said the mountain's first eruption in nearly 40 years may soo… [+1469 chars]"
+               }[]
+        */
+        const news: NewsItem[] = data.length >= 10 ? data.filter((_, idx: number) => idx < 10) : data;
 
-        const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement | null = document.querySelector('#newsItemTemp');
+        if (!newsItemTemp) throw new Error('Element #newsItemTemp is missing');
 
-        news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+        news.forEach((item: NewsItem, idx: number) => {
+            const newsClone: HTMLTemplateElement = newsItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+            if (!newsClone) return;
+            const setTextContent = (selector: string, textContent: string): void => {
+                const element: HTMLTemplateElement | null = newsClone.querySelector(selector);
+                if (!element) throw new Error(`Element ${selector} is missing`);
+                element.textContent = textContent;
+            };
+            if (idx % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            const metaPhoto: HTMLTemplateElement | null = newsClone.querySelector('.news__meta-photo');
+            if (metaPhoto) metaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
+            setTextContent('.news__meta-author', item.author || item.source.name);
+            setTextContent('.news__meta-date', item.publishedAt.slice(0, 10).split('-').reverse().join('-'));
+            setTextContent('.news__description-title', item.title);
+            setTextContent('.news__description-source', item.source.name);
+            setTextContent('.news__description-content', item.description);
 
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+            const readMoreAnchor: HTMLTemplateElement | null = newsClone.querySelector('.news__read-more a');
+            if (readMoreAnchor) readMoreAnchor.setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
-
-        document.querySelector('.news').innerHTML = '';
-        document.querySelector('.news').appendChild(fragment);
+        const newsElement: HTMLTemplateElement | null = document.querySelector('.news');
+        if (newsElement) {
+            newsElement.innerHTML = '';
+            newsElement.appendChild(fragment);
+        }
     }
 }
 
