ef103c1e7410e4f3eeee80c8906eb04c8aaeba72
EternalRival <eternalrival8@gmail.com>
2022-12-12 14:03:51 +0300

    feat: change apikey

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,10 +2,11 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.up.railway.app/', { apiKey: 'b5346c6ecdb64f0e898b5e738fcf769a' });
+        super('https://nodenews.up.railway.app/', { apiKey: '9507b1036dba4614aa8a79a67bc2f599' });
     }
 }
 
 export default AppLoader;
 
 // 63383ad8c3164b8abdedaeb401f3bff8
+// b5346c6ecdb64f0e898b5e738fcf769a
